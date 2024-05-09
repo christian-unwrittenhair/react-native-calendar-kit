b41752d4f5afa7be733c305a06d0d3bba1650fcc
christian-unwrittenhair <140989733+christian-unwrittenhair@users.noreply.github.com>
2024-05-09 19:34:02 +0800

    Update MultipleDayBar.tsx

--- src/components/Timeline/TimelineHeader/MultipleDayBar.tsx
+++ src/components/Timeline/TimelineHeader/MultipleDayBar.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
-import { times } from 'lodash';
+import times from 'lodash/times';
 import moment from 'moment-timezone';
 import { COLUMNS, DEFAULT_PROPS } from '../../../constants';
 import type { DayBarItemProps } from '../../../types';
@@ -8,57 +8,42 @@ import { getDayBarStyle } from '../../../utils';
 
 export const SCREEN_WIDTH = Dimensions.get("window").width;
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  dayItem: {
-    alignItems: 'center',
-  },
-  dayNumBtn: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: (SCREEN_WIDTH - 88) / 7,
-    width: (SCREEN_WIDTH - 88) / 7,
-    backgroundColor: DEFAULT_PROPS.WHITE_COLOR,
-  },
-  dayName: {
-    color: DEFAULT_PROPS.SECONDARY_COLOR,
-    fontSize: 10,
-  },
-  dayNumber: {
-    color: DEFAULT_PROPS.SECONDARY_COLOR,
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-  dotView: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    height: 4,
-    width: "100%",
-  },
-});
+const Dots = ({ date, highlightDates }) => {
+  const count = highlightDates.reduce((acc, curr) => ({
+    ...acc,
+    [curr.date]: curr.count,
+  }), {})[date];
 
-const Dots = ({ date, highlightDates }: any) => {
-  const dotElements = useMemo(() => {
-    const count = highlightDates[date]?.count || 0;
-    const dotArray = Array.from({ length: Math.min(3, Math.floor(count / 2) + 1) }, (_, i) => (
-      <View key={i} style={{
-        width: 4,
-        height: 4,
-        borderRadius: 2,
-        backgroundColor: count % 2 === 0 ? "#F06D76" : '#56CCF2',
-        marginHorizontal: 2,
-      }} />
-    ));
+  const overlapArr = (() => {
+    if (count === null || count === undefined) {
+      return [];
+    } else if (count >= 1 && count <= 2) {
+      return [1];
+    } else if (count >= 3 && count <= 5) {
+      return [1, 2];
+    } else {
+      return [1, 2, 3];
+    }
+  })();
 
-    return dotArray;
-  }, [date, highlightDates]);
+  const getColor = (count, index) => {
+    switch (count) {
+      case 0:
+        return "#F06D76";
+      case 1:
+        return '#56CCF2';
+      default:
+        return index % 2 === 0 ? "#F06D76" : '#56CCF2';
+    }
+  };
 
-  return <View style={styles.dotView}>{dotElements}</View>;
+  return (
+    <View style={styles.dotView}>
+      {overlapArr.map((ele, index) => (
+        <View key={index} style={{ width: 4, height: 4, borderRadius: 2, backgroundColor: getColor(count, index), marginHorizontal: index === highlightDates.length - 1 || index === 4 ? 0 : 2 }} />
+      ))}
+    </View>
+  );
 };
 
 const MultipleDayBar = ({
@@ -71,39 +56,86 @@ const MultipleDayBar = ({
   locale,
   highlightDates,
   currentDate,
+  tzOffset,
 }: DayBarItemProps) => {
-  const renderDay = useMemo(() => (dayIndex: number) => {
-    const dateByIndex = moment.tz(startDate, currentDate).add(dayIndex, 'days');
+  const _renderDay = (dayIndex: number) => {
+    const dateByIndex = moment.tz(startDate, tzOffset).add(dayIndex, 'd');
     const dateStr = dateByIndex.format('YYYY-MM-DD');
-    const [dayNameText, dayNum] = dateByIndex.locale(locale).format('dd,D').split(',');
+    const [dayNameText, dayNum] = dateByIndex
+      .locale(locale)
+      .format('dd,D')
+      .split(',');
 
-    const { dayName, dayNumber, dayNumberContainer } = getDayBarStyle(currentDate, dateByIndex, theme);
+    const { dayName, dayNumber, dayNumberContainer } = getDayBarStyle(
+      currentDate,
+      dateByIndex,
+      theme,
+    );
 
     return (
-      <View key={dateStr} style={[styles.dayItem, { width: columnWidth }]}>
+      <View
+        key={`${startDate}_${dayIndex}`}
+        style={[styles.dayItem, { width: columnWidth }]}
+      >
         <TouchableOpacity
           activeOpacity={1}
           disabled={!onPressDayNum}
           onPress={() => onPressDayNum?.(dateStr)}
           style={[styles.dayNumBtn, dayNumberContainer]}
         >
-          <Text allowFontScaling={theme.allowFontScaling} style={[styles.dayName, dayName]}>
+          <Text
+            allowFontScaling={theme.allowFontScaling}
+            style={[styles.dayName, dayName]}
+          >
             {dayNameText}
           </Text>
-          <Text allowFontScaling={theme.allowFontScaling} style={[styles.dayNumber, dayNumber]}>
+          <Text
+            allowFontScaling={theme.allowFontScaling}
+            style={[styles.dayNumber, dayNumber]}
+          >
             {dayNum}
           </Text>
           <Dots date={dateStr} highlightDates={highlightDates} />
         </TouchableOpacity>
       </View>
     );
-  }, [startDate, viewMode, columnWidth, onPressDayNum, theme, locale, highlightDates, currentDate]);
+  };
 
   return (
-    <View style={[styles.container, { width }]}>
-      {times(COLUMNS[viewMode], renderDay)}
+    <View
+      style={[
+        styles.container,
+        { width, height: DEFAULT_PROPS.DAY_BAR_HEIGHT },
+      ]}
+    >
+      {times(COLUMNS[viewMode]).map(_renderDay)}
     </View>
   );
 };
 
 export default MultipleDayBar;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  dayItem: { alignItems: 'center' },
+  dayNumBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    height: (SCREEN_WIDTH - 88) / 7,
+    width: (SCREEN_WIDTH - 88) / 7,
+    backgroundColor: DEFAULT_PROPS.WHITE_COLOR,
+  },
+  dayName: { color: DEFAULT_PROPS.SECONDARY_COLOR, fontSize: 10 },
+  dayNumber: { color: DEFAULT_PROPS.SECONDARY_COLOR, fontSize: 14, fontWeight: 'bold' },
+  dotView: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 4,
+    width: "100%",
+  },
+});
