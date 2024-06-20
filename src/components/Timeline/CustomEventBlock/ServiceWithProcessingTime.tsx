614d8b6858494e49dec8845cc02a81eb0c32b25a
Mykel Aballe <mykel@unwritten.hair>
2024-06-20 20:16:14 +0800

    revert refactor of useAnimatedStyles in ServiceWithProcessingTime component

--- src/components/Timeline/CustomEventBlock/ServiceWithProcessingTime.tsx
+++ src/components/Timeline/CustomEventBlock/ServiceWithProcessingTime.tsx
@@ -18,52 +18,56 @@ const ServiceWithProcessingTime = ({
 		eventAnimatedDuration
 	} = useTimelineCalendarContext();
 
-	const animatedStyles = useAnimatedStyle(() => {
-		const baseHeight = (event.duration * heightByTimeInterval.value) / totalSlotDuration;
-	
-		const applicationHeight = baseHeight * service.application_time;
-		const processingHeight = baseHeight * service.processing_time;
-		const finishingHeight = baseHeight * service.finishing_time;
-	
+	  const applicationTimeStyles = useAnimatedStyle(() => {
+		const height = ((event.duration * heightByTimeInterval.value) / totalSlotDuration) * service.application_time
+
 		return {
-		  application: {
-			height: withTiming(applicationHeight, {
-			  duration: eventAnimatedDuration
-			}),
-		  },
-		  processing: {
-			height: withTiming(processingHeight, {
-			  duration: eventAnimatedDuration
-			}),
-		  },
-		  finishing: {
-			height: withTiming(finishingHeight, {
-			  duration: eventAnimatedDuration
-			}),
-		  }
-		};
-	  }, [event]);
+			height: withTiming(height, {
+				duration: eventAnimatedDuration
+			})
+		}
+	  }, [event, heightByTimeInterval])
+
+	  const processingTimeStyles = useAnimatedStyle(() => {
+		const height = ((event.duration * heightByTimeInterval.value) / totalSlotDuration) * service.processing_time
+
+		return {
+			height: withTiming(height, {
+				duration: eventAnimatedDuration
+			})
+		}
+	  }, [event, heightByTimeInterval])
+
+	  const finishingTimeStyles = useAnimatedStyle(() => {
+		const height = ((event.duration * heightByTimeInterval.value) / totalSlotDuration) * service.finishing_time
+
+		return {
+			height: withTiming(height, {
+				duration: eventAnimatedDuration
+			})
+		}
+	  }, [event, heightByTimeInterval])
 
 	const handlePressProcessingTime = () => onPressEventService?.(event, service)
 	
 	return (
 		<View style={{ flexDirection: "column" }}>
 			<Animated.View
-				style={animatedStyles.application}
+				style={applicationTimeStyles}
 			>
 				{header}
 			</Animated.View>
 
 			<TouchableOpacity disabled={!onPressEventService} onPress={handlePressProcessingTime}>
 				<Animated.View
-					style={animatedStyles.processing}
+					style={processingTimeStyles}
 				>
 					<CustomLineView strokeColor="white" strokeWidth="2" width={"100%"} height={100} />
 				</Animated.View>
 			</TouchableOpacity>
 
 			<Animated.View
-				style={animatedStyles.finishing}
+				style={finishingTimeStyles}
 			/>
 		</View>
 	);
